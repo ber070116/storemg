4724d64fb3428961f9537a52762a6081f80dcaf6
ber070116 <brandomhk@gmail.com>
2017-08-04 12:52:32 -0400

    Modelo de usuario basico

--- storemg/settings.py
+++ storemg/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user'
+    'apps.user'
 ]
+
+AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
